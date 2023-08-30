4bdeddd3a65cf4e27a12b44656414be1d5b7c7d6
Shkrelic <60494974+Shkrelic@users.noreply.github.com>
2023-08-30 15:47:16 -0400

    Update vuln_chk2.sh

--- vuln_chk2.sh
+++ vuln_chk2.sh
@@ -1,41 +1,35 @@
 #!/bin/bash
 
-# Color codes for output
+# Color codes
 RED='\033[0;31m'
 GREEN='\033[0;32m'
-RESET='\033[0m'
+NC='\033[0m' # No Color
 
-# Step 1: Input Validation
+# Input validation
 if [ -z "$1" ] || [ -z "$2" ]; then
-    echo -e "${RED}Both arguments \$1 and \$2 must be defined.${RESET}"
+    echo -e "${RED}Error: Please provide both arguments.${NC}"
+    echo "Usage: $0 <hosts_file> <advisory>"
     exit 1
 fi
 
-# Step 2: Connection Verification
-verify_connection() {
-    local hosts_file="$1"
-    local responsive_hosts=()
+host_file=$1
+advisory=$2
 
-    # Unique string for connection test
-    unique_string="CONNECTION_SUCCESS_12345"
+# Connection verification
+while IFS= read -r host; do
+    pssh.sh -h $host "echo connected" &> /dev/null
+    if [ $? -ne 0 ]; then
+        echo -e "${RED}${host} - CONNECTION ERROR${NC}"
+    else
+        echo -e "${GREEN}${host} is reachable.${NC}"
+    fi
+done < "$host_file"
 
-    # Using pssh.sh to check connection by echoing the unique string
-    results=$(~/bin/pssh.sh -f "$hosts_file" echo "$unique_string")
+# API interaction via another server
+SERVER_TO_MAKE_API_CALL="your.server.name"
+data=$(pssh.sh -h "$SERVER_TO_MAKE_API_CALL" "curl -s 'https://access.redhat.com/hydra/rest/securitydata/cve.json?advisory=$advisory'")
+cve_data=$(echo "$data" | jq '.[] | "\(.CVE) - \(.severity)"')
 
-    # Process results and categorize hosts as responsive or unresponsive
-    while IFS= read -r line; do
-        host=$(echo $line | cut -d' ' -f1)  # Extracting the hostname
-        if [[ $line == *"$unique_string"* ]]; then
-            responsive_hosts+=("$host")
-        else
-            echo -e "${RED}${host} - CONNECTION ERROR${RESET}"
-        fi
-    done <<< "$results"
+echo -e "${GREEN}CVE Details from Server:${NC} $cve_data"
 
-    echo "${responsive_hosts[@]}"
-}
-
-# Later, when calling this function
-responsive_hosts=( $(verify_connection "$1") )
-
-# You can then use $responsive_hosts for subsequent steps in the script.
+# Continue with other tasks...
